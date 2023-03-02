3acfc36194fee62a9b74a493141adea0aef63719
gajjarv <vishalg@berkeley.edu>
2023-03-01 21:07:26 -0800

    Dual image with spectra

--- SNR_RA_DEC.py
+++ SNR_RA_DEC.py
@@ -23,6 +23,21 @@ class Point:
         self.dec = dec
         self.snr = snr
 
+def get_onspec(fb,f_start_on,f_stop_on,f_start_off,f_stop_off): #Pass filterbank object name 
+        onfb = fb.grab_data(f_start=f_start_on,f_stop=f_stop_on)
+        offfb = fb.grab_data(f_start=f_start_off,f_stop=f_stop_off)
+
+        onspec = np.mean(onfb[1],axis=0)
+        offspec = np.mean(offfb[1],axis=0)
+
+        onspec = (savgol_filter(onspec,11,4))
+        offspec = (savgol_filter(offspec,11,4))
+
+        onspec = onspec-np.mean(offspec)
+        onspec = onspec/np.std(offspec)
+
+        return onspec
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some arguments.')
@@ -42,7 +57,7 @@ if __name__ == "__main__":
     beams = []
 
     fil_pattern = args.fil
-    fil_pattern = fil_pattern + "*"
+    fil_pattern = fil_pattern + "*.fil"
     fil_list = glob.glob(fil_pattern)
 
     for fil in fil_list:
@@ -51,31 +66,47 @@ if __name__ == "__main__":
         snr = SNR(fb,f_start_on,f_stop_on,f_start_off,f_stop_off)
         beams.append(Point(fb.header['src_raj'],fb.header['src_dej'],snr))
 
+    max_snr = max([point.snr for point in beams])
+    min_snr = min([point.snr for point in beams])
+    half_max_snr = 0.5 * max_snr
+
+    max_snr_fil = fil_list[np.argmax(snrs)]
+    min_snr_fil = fil_list[np.argmin(snrs)]
+    half_max_snr_fil = fil_list[np.argmin(np.abs(snrs - half_max_snr))]
+
+    onspec_max = get_onspec(wt(max_snr_fil),f_start_on,f_stop_on,f_start_off,f_stop_off)
+    onsepc_min = get_onspec(wt(min_snr_fil),f_start_on,f_stop_on,f_start_off,f_stop_off)	
+    onspec_hp  = get_onspec(wt(half_max_snr_fil),f_start_on,f_stop_on,f_start_off,f_stop_off)
+
     # Convert RA and DEC coordinates of the Point objects into arrays
     ra_hourangle = np.array([beam.ra.value for beam in beams])
     dec_deg = np.array([beam.dec.to('degree').value for beam in beams])
     snrs = np.array([beam.snr for beam in beams])
 	
-	# interpolate data onto a regular grid
+    # interpolate data onto a regular grid
     xi = np.linspace(ra_hourangle.min(), ra_hourangle.max(), 100)
     yi = np.linspace(dec_deg.min(), dec_deg.max(), 100)
     zi = griddata((ra_hourangle, dec_deg), snrs, (xi[None,:], yi[:,None]), method='cubic')
 	
-    # create contour plot
-    fig, ax = plt.subplots(figsize=(10, 10))
+    # create contour plot and frequency vs power subplot
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
     cmap = 'viridis'
     levels = np.linspace(snrs.min(), snrs.max(), 20)
-    contour_set = ax.contour(xi, yi, zi, levels=levels, cmap=cmap)
-    ax.scatter(ra_hourangle,dec_deg)
-
-    # create colorbar
-    cbar = plt.colorbar(contour_set, ax=ax)
+    
+    # plot RA vs DEC contour
+    contour_set = axs[0].contour(xi, yi, zi, levels=levels, cmap=cmap)
+    axs[0].scatter(ra_hourangle, dec_deg)
+    cbar = plt.colorbar(contour_set, ax=axs[0])
     cbar.ax.set_ylabel('SNR')
-	
-    # set axis labels
-    ax.set_xlabel('RA [h]')
-    ax.set_ylabel('DEC [deg]')
-	
-    plt.savefig('Grid_pattern_RA_DEC.png')
-
+    axs[0].set_xlabel('RA [h]')
+    axs[0].set_ylabel('DEC [deg]')
+    
+    # plot Frequency vs Power
+    axs[1].plot(onspec_max)
+    axs[1].plot(onspec_min)
+    axs[1].plot(onspec_hp)
+    axs[1].set_xlabel('Frequency [MHz]')
+    axs[1].set_ylabel('Power')
+    
+    plt.savefig('Grid_pattern_RA_DEC_Freq_Power.png')
 
